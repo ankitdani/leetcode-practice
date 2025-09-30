a006fad0d5edc6067c30c4fb88f891f7869e2da4
unknown <ankitdani1997@gmail.com>
2025-09-29 21:32:55 -0700

    Added time and space complexity

--- 100. Same Tree.py
+++ 100. Same Tree.py
@@ -20,6 +20,11 @@ if we reach the end of the tree then return true
 invalid conditions are value not same, 1 of the node is null
 '''
 
+'''
+Time: O(m+n)
+Space: O(1)
+'''
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
