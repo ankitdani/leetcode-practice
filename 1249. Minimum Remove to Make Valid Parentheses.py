fb508eb0cfb3b13dc70a55c50b351bdb14b56053
unknown <ankitdani1997@gmail.com>
2025-10-07 16:39:23 -0700

    Implement using stack

---
+++ 1249. Minimum Remove to Make Valid Parentheses.py
@@ -0,0 +1,56 @@
+'''
+Given a string s of '(' , ')' and lowercase English characters.
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+'''
+
+'''
+input = ab(c))
+stack = []
+
+ch = a
+stack = 
+
+ch = b
+stack = 
+
+ch = (
+stack = 2
+
+ch = )
+stack = pop 
+
+ch = )
+stack = 5
+
+2nd pass for marking remaining indexes of stack as empty
+
+result => ab(c)
+'''
+
+'''
+Time: O(n)
+Space: O(n)
+'''
+
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        s = list(s)
+        stack = []
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            elif s[i] == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ''
+        while stack:
+            s[stack.pop()] = ''
+        return ''.join(s)
